29161eb6fba798e9db3b48b1d41b88d84d0746b0
mercyblitz <mercyblitz@gmail.com>
2012-07-05 03:46:22 +0000

    [更新日志] 2012/07/05: 1.CSS输出乱码问题 2.调整宝贝选择器界面中“已选择”的宝贝出现顺序，将按照用户选择的顺序。 3.解决ptype=number时输出字符的问题，目前ptype=number后，将输出数字类型，未来支持更多ptype格式。 4.为Mac SDK启动脚本startServer.sh提供添加配置信息，与Windows功能保持一致。

--- TAE_SDK/bin/startServer.sh
+++ TAE_SDK/bin/startServer.sh
@@ -18,7 +18,13 @@ start_jar_found(){
 
 java_found(){
 	JETTY_HOME=$DCSDK_HOME/lib/jetty
-	JAVA_OPTS="-DDCSDK_HOME=$DCSDK_HOME -Dmain.class=com.taobao.tae.sdk.platform.Main -Djava.io.tmpdir=$DCSDK_HOME/temp -DSTART=$DCSDK_HOME/conf/start.config -Dassets.host=a.tbcdn.cn"
+	JAVA_OPTS="$JAVA_OPTS -DDCSDK_HOME=$DCSDK_HOME"
+	JAVA_OPTS="$JAVA_OPTS -Dmain.class=com.taobao.tae.sdk.platform.Main"
+	JAVA_OPTS="$JAVA_OPTS -Djava.io.tmpdir=$DCSDK_HOME/temp"
+	JAVA_OPTS="$JAVA_OPTS -DSTART=$DCSDK_HOME/conf/start.config"
+	JAVA_OPTS="$JAVA_OPTS -Dassets.host=a.tbcdn.cn"
+	JAVA_OPTS="$JAVA_OPTS -Dkissy.uri=/s/kissy/1.2.0/kissy-min.js"
+	JAVA_OPTS="$JAVA_OPTS -Dsystem.module.common.js.uri=/apps/taesite/platinum/scripts/common/mods/shop/"
 	START_JAR=""
 	for _START_JAR in $JETTY_HOME/start*.jar
 	do
