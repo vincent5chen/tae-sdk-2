fb97e05f4bfeb1eb4c0bbd35ec733723fd42d92f
mercyblitz <mercyblitz@gmail.com>
2012-08-24 13:30:10 +0000

    [更新日志] 2012/08/24: 1.[SDK工具-数据接口升级] 提供折扣价格，简单使用说明： 	调用方法：$item->discountPrice 	该数据接口返回的折扣价格，在SDK中是随机数，在线上是真实的折扣价格。 	特别注意：该接口可能返回null，表示没有折扣价，设计师应该注意代码写法。 	例如：if($item->discountPrice) { 			//说明折扣价存在，编写逻辑 		  }
    2.[SDK工具-软件升级] 升级PHP渲染引擎，与线上系统保持一致
    					 
    3.[SDK工具-问题修复] 
    	a) #hd和#bd之间的10px问题。在10px和0px之间，设计师可二选一的方式，控制间距	
    	b) 修复宝贝详情页添加模块的问题
    	c) 修复切换行业数据后，已选择的宝贝选择器问题
    	d) 修复风格管理中，保存后，图片路径自动替换成全路径的问题，影响上传后的图片不显示
    	e) 修复切换行业数据后，前行业宝贝遗留的问题
    4.[SDK工具-系统优化] 
    	a)减少软件内存和磁盘占用空间，节约网络带宽
    	b)系统模块同步优化，容错性更强
    	c)取消模板缩率图等强制验证

--- TAE_SDK/bin/DebugServer.bat
+++ TAE_SDK/bin/DebugServer.bat
@@ -22,22 +22,21 @@ set DEBUG_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=
 set JETTY_HOME=%DCSDK_HOME%\lib\jetty
 set JAVA_OPTS=-DDCSDK_HOME=%DCSDK_HOME%
 set JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=GBK
-set JAVA_OPTS=%JAVA_OPTS% -Dmain.class=com.taobao.tae.sdk.platform.Main 
-set JAVA_OPTS=%JAVA_OPTS% -DSTART=%DCSDK_HOME%\conf\start.config 
-set JAVA_OPTS=%JAVA_OPTS% -Djava.io.tmpdir=%DCSDK_HOME%\temp 
+set JAVA_OPTS=%JAVA_OPTS% -Dmain.class=com.taobao.tae.sdk.platform.Main
+set JAVA_OPTS=%JAVA_OPTS% -DSTART=%DCSDK_HOME%\conf\start.config
+set JAVA_OPTS=%JAVA_OPTS% -Djava.io.tmpdir=%DCSDK_HOME%\temp
 set JAVA_OPTS=%JAVA_OPTS% -Duser.home=%DCSDK_HOME%\temp
 set JAVA_OPTS=%JAVA_OPTS% -Duser.dir=%DCSDK_HOME%\temp
 
 set JAVA_OPTS=%JAVA_OPTS% -Dassets.host=a.tbcdn.cn
 set JAVA_OPTS=%JAVA_OPTS% -Dkissy.uri=/s/kissy/1.2.0/kissy-min.js
 set JAVA_OPTS=%JAVA_OPTS% -Dsystem.module.common.js.uri=/apps/taesite/platinum/scripts/common/mods/
-set JAVA_OPTS=%JAVA_OPTS% -Dcaja.service.uri=http://zxn.daily.taobao.net/tbcajaService.htm
-set JAVA_OPTS=%JAVA_OPTS% -Dsite.admin.root.url=http://siteadmin.daily.taobao.net
-set JAVA_OPTS=%JAVA_OPTS% -Dsystem.modules.list.sync.url=http://siteadmin.daily.taobao.net/module/singleModule.htm?want=all
+set JAVA_OPTS=%JAVA_OPTS% -Dcaja.service.uri=http://zxn.taobao.com/tbcajaService.htm
+set JAVA_OPTS=%JAVA_OPTS% -Dsite.admin.root.url=http://siteadmin.taobao.com
+set JAVA_OPTS=%JAVA_OPTS% -Duser.nick=c≤‚ ‘’À∫≈154
 
 set JAVA_OPTS=%JAVA_OPTS% -Ddevelopment.mode=false
 set JAVA_OPTS=%JAVA_OPTS% -Denable.sdk.mode=true
-
 
 
 
