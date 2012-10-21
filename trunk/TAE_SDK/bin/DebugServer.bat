f2a5db445fea69c313a6c144fc0a53593d251fd1
mercyblitz@gmail.com <mercyblitz@gmail.com@74645254-29b1-930f-00a9-5d0627b03fd4>
2012-10-21 07:49:58 +0000

    2012/10/21: 1.[SDK工具-调整优化] 	a) [仅启动NewStartServer.bat时] 内存使用率优化，即当CPU闲暇时，清理SDK占用的内存
    2.[SDK工具-问题修复]
    	a) 修复宝贝选择器数据加载失败的问题

--- trunk/TAE_SDK/bin/DebugServer.bat
+++ trunk/TAE_SDK/bin/DebugServer.bat
@@ -51,6 +51,11 @@ set JAVA_OPTS=%JAVA_OPTS% -Denable.sdk.mode=true
 
 set JAVA_OPTS=%JAVA_OPTS% -Ddisplay.template.number.per.page=10
 
+set JAVA_OPTS=%JAVA_OPTS% -Doffline.mode
+
+set JAVA_OPTS=%JAVA_OPTS% -Dprmain.async.action.enabled
+set JAVA_OPTS=%JAVA_OPTS% -Dprmain.async.action.thread.pool.size=5
+
 
 
 
